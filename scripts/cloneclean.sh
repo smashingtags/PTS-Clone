d44264f45a82e0ba65188bff3d9c7b03f813a25d
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-19 00:14:51 +0200

    Update cloneclean.sh

--- scripts/cloneclean.sh
+++ scripts/cloneclean.sh
@@ -12,18 +12,22 @@ cloneclean() {
     hdpath="$(cat /var/plexguide/server.hd.path)"
     cleaner="$(cat /var/plexguide/cloneclean)"
 
+    #permissions part for clonecleaner */move folder*
+    find "$hdpath/move/" -mindepth 1 -type d -exec chmod -R 775 {} \+
+    find "$hdpath/move/" -mindepth 1 -type f -exec chmod -R 755 {} \+
+
     #NOTE NZB CLIENTS USED THEN SAME NOW
-    find "$hdpath/downloads/nzb" -mindepth 1 -type f -cmin +$cleaner -size -3G 2>/dev/null -exec rm -rf {} \;
-    find "$hdpath/nzb/" -mindepth 1 -name "*.nzb.*" -type f -cmin +$cleaner 2>/dev/null -exec rm -rf {} \;
+    find "$hdpath/downloads/nzb" -mindepth 1 -type f -cmin +$cleaner -size -3G 2>/dev/null -exec rm -rf  \{\} \;
+    find "$hdpath/nzb/" -mindepth 1 -name "*.nzb.*" -type f -cmin +$cleaner 2>/dev/null -exec rm -rf {}  \{\} \;
 
     #NOTE TORRENT CLIENTS USED THE SAME NOW
-    find "$hdpath/downloads/torrent" -mindepth 3 -type d -cmin +$cleaner -size -3G 2>/dev/null -exec rm -rf {} \;
+    find "$hdpath/downloads/torrent" -mindepth 3 -type d -cmin +$cleaner -size -3G 2>/dev/null -exec rm -rf  \{\} \;
 
     # Remove empty directories
-    find "$hdpath/move" -mindepth 2 -type d -empty -delete
+    find "$hdpath/move" -mindepth 2 -type d -empty -exec rmdir \{} \;
 
     #DO NOT decrease DEPTH on this, leave it at 3. Leave this alone!
-    find "$hdpath/downloads" -mindepth 2 -type d \( ! -name syncthings ! -name .stfolder \) -empty -delete
+    find "$hdpath/downloads" -mindepth 3 -type d \( ! -name syncthings ! -name .stfolder \) -empty -delete
 
     # Prevents category folders underneath the downloaders from being deleted, while removing empties from the import process.
     # This was done to address some apps having an issue if the category underneath the downloader is missing.
