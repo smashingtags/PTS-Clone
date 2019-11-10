6389bb315628ed3a76d626b2e3ff3e02db2b4dcf
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-10 04:16:16 +0100

    Update oauth.sh

--- functions/oauth.sh
+++ functions/oauth.sh
@@ -45,8 +45,8 @@ EOF
   echo "client_id = $pgclonepublic" >>/opt/appdata/plexguide/.${type}
   echo "client_secret = $pgclonesecret" >>/opt/appdata/plexguide/.${type}
   echo "type = drive" >>/opt/appdata/plexguide/.${type}
-  echo -n "token = {\"access_token\":${accesstoken}\"token_type\":\"Bearer\",\"refresh_token\":${refreshtoken}\"expiry\":\"${final}\"}" >>/opt/appdata/plexguide/.${type}
   echo "server_side_across_configs = true" >>/opt/appdata/plexguide/.${type}
+  echo -n "token = {\"access_token\":${accesstoken}\"token_type\":\"Bearer\",\"refresh_token\":${refreshtoken}\"expiry\":\"${final}\"}" >>/opt/appdata/plexguide/.${type}
   echo "" >>/opt/appdata/plexguide/.${type}
   if [ "$type" == "tdrive" ]; then
     teamid=$(cat /var/plexguide/pgclone.teamid)
