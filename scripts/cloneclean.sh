b1fb0427a5b6227f8eef92292386042b1367d3f9
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-30 01:37:51 +0100

    Update cloneclean.sh

--- scripts/cloneclean.sh
+++ scripts/cloneclean.sh
@@ -15,27 +15,22 @@ cloneclean() {
     ((nzb=${cleaner}/3))
 
     #permissions part for clonecleaner */move folder*
-    find "$(cat /var/plexguide/server.hd.path)/move" -mindepth 1 -exec chmod -cR 755 \{}\;
-    find "$(cat /var/plexguide/server.hd.path)/move" -mindepth 1 -exec chown -cR 1000:1000 \{}\;
-
-    #permissions part for clonecleaner */downloads folder*#    
-    find "$(cat /var/plexguide/server.hd.path)/downloads/" -mindepth 1 -exec chmod -cR 755 \{}\;
-    find "$(cat /var/plexguide/server.hd.path)/downloads/" -mindepth 1 -exec chown -cR 1000:1000 \{}\;
+    find "$(cat /var/plexguide/server.hd.path)/move" -mindepth 1 -exec chmod -cR 755 {} \+
+    find "$(cat /var/plexguide/server.hd.path)/move" -mindepth 1 -exec chown -cR 1000:1000 {} \+
+    find "$(cat /var/plexguide/server.hd.path)/downloads/" -mindepth 1 -exec chmod -cR 755 {} \+
+    find "$(cat /var/plexguide/server.hd.path)/downloads/" -mindepth 1 -exec chown -cR 1000:1000 {} \+
 
     #NOTE NZB CLIENTS USED THEN SAME NOW
     find "$(cat /var/plexguide/server.hd.path)/downloads/nzb/" -mindepth 1 -type d -mmin +"$(cat /var/plexguide/cloneclean)" -exec rm -rf \{} \;
     find "$(cat /var/plexguide/server.hd.path)/downloads/nzb/" -mindepth 1 -type f -size -10M -mmin +2 -exec rm -rf \{} \;
     nzbremoverunwantedfiles
     find "$(cat /var/plexguide/server.hd.path)/nzb/" -mindepth 1 -name "*.nzb.*" -type f -mmin +"$nzb" -exec rm -rf \{} \;
-
     #NOTE TORRENT CLIENTS USED THE SAME NOW
     find "$(cat /var/plexguide/server.hd.path)/downloads/torrent" -mindepth 2 -type d -mmin +"$torrent" -exec rm -rf \{} \;
     #DO NOT decrease DEPTH on this, leave it at 3. Leave this alone!
     find "$(cat /var/plexguide/server.hd.path)/move" -mindepth 2 -type d -empty -exec rmdir \{} \;
     find "$(cat /var/plexguide/server.hd.path)/downloads" -mindepth 3 -type d \( ! -name syncthings ! -name .stfolder \) -empty -exec rm -rf \{} \;
 
-    # Prevents category folders underneath the downloaders from being deleted, while removing empties from the import process.
-    # This was done to address some apps having an issue if the category underneath the downloader is missing.
     # Prevents category folders underneath the downloaders from being deleted, while removing empties from the import process.
     # This was done to address some apps having an issue if the category underneath the downloader is missing.
     find "$(cat /var/plexguide/server.hd.path)/downloads" -mindepth 2 -type d \( ! -name .stfolder ! -name **games** ! -name ebooks ! -name abooks ! -name sonarr** ! -name radarr** ! -name lidarr** ! -name **kids** ! -name **tv** ! -name **movies** ! -name music** ! -name audio** ! -name anime** ! -name software ! -name xxx ! -name **nzb** ! -name **torrent** \) -empty -exec rm -rf \{} \;
@@ -108,7 +103,7 @@ UNWANTED_FILES=(
 FIND=$(which find)
 FIND_BASE_CONDITION='-type f'
 FIND_ADD_NAME='-o -name'
-FIND_ACTION=' -delete'
+FIND_ACTION='-delete'
 
 #Folder Setting
 TARGET_FOLDER=$1"$(cat /var/plexguide/server.hd.path)/downloads/"
@@ -169,3 +164,4 @@ removefilestdrive && nzbremoverunwantedfiles
 else removefilesgdrive && nzbremoverunwantedfiles; fi
 }
 runner
+cloneclean
