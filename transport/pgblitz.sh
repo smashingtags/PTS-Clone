805c25e6bcb2fdfd73d4fc19008dbd45f7eaa8e0
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2020-01-08 22:22:44 +0100

    file upload gdrive add
    if file -name *.srt -o -name *.idx -o -name *.sub
    will upload to gdrive ( tests running actually

--- transport/pgblitz.sh
+++ transport/pgblitz.sh
@@ -20,9 +20,12 @@ startscript() {
       --exclude-from="/opt/pgclone/excluded/excluded.folder"
     echo "" >>/var/plexguide/logs/pgblitz.log
     echo " -- Starting Blitz cycle $cyclecount - $p: $(date "+%Y-%m-%d %H:%M:%S") --" >>/var/plexguide/logs/pgblitz.log
-      if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f | wc -l ) -eq 0 ]]; then rczero; fi
+      if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f | wc -l ) -lt 1 ]]; then rczero; fi
       if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f | wc -l ) -eq 1 ]]; then rcone; fi
       if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f | wc -l ) -gt 1 ]]; then rcupload; fi
+	  ###adon later
+	  #if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f \( -name *.srt -o -name *.idx -o -name *.sub \) ) ]]; then rcdrive;fi
+	  ###adon later
     echo " -- Completed cycle $cyclecount: $(date "+%Y-%m-%d %H:%M:%S")---" >>/var/plexguide/logs/pgblitz.log
     echo "$(tail -n 200 /var/plexguide/logs/pgblitz.log)" >/var/plexguide/logs/pgblitz.log
  done </var/plexguide/.blitzfinal
@@ -61,6 +64,43 @@ rcupload() {
     --exclude-from="/opt/pgclone/excluded/excluded.folder"
   echo " -- Upload has finished --" >>/var/plexguide/logs/pgblitz.log
  done </var/plexguide/.blitzfinal
+}
+rcdrive() {
+   useragent="$(cat /var/plexguide/uagent)"
+   bwlimit="$(cat /var/plexguide/blitz.bw)"
+   vfs_dcs="$(cat /var/plexguide/vfs_dcs)"
+   vfs_mt="$(cat /var/plexguide/vfs_mt)"
+   vfs_t="$(cat /var/plexguide/vfs_t)"
+   vfs_c="$(cat /var/plexguide/vfs_c)"
+   catter="$(cat /opt/appdata/plexguide/rclone.conf | grep "GDSA01C" )"
+
+if [[ "$catter" == "[GDSA01C]" ]]; then
+ rclone moveto "$(cat /var/plexguide/server.hd.path)/move" "gcrypt:/" \
+    --config=/opt/appdata/plexguide/rclone.conf \
+    --log-file=/var/plexguide/logs/pgblitz.log \
+    --log-level=INFO --stats=5s --stats-file-name-length=0 \
+    --max-size=300G --min-age 1m --tpslimit=8 \
+    --drive-pacer-min-sleep=100ms --checkers=8 \
+    --transfers="$vfs_t" --no-traverse \
+    --fast-list --max-transfer "$vfs_mt" --bwlimit="$bwlimit" \
+    --drive-chunk-size="$vfs_dcs" --user-agent="$useragent" \
+    --include="*.srt*" --include="*.idx" --include="*.sub"
+  echo " -- Subs Upload has finished to GDCRYPT:--" >>/var/plexguide/logs/pgblitz.log
+  fi
+
+if [[ "$catter" != "[GDSA01C]" ]]; then
+ rclone moveto "$(cat /var/plexguide/server.hd.path)/move" "gdrive:/" \
+    --config=/opt/appdata/plexguide/rclone.conf \
+    --log-file=/var/plexguide/logs/pgblitz.log \
+    --log-level=INFO --stats=5s --stats-file-name-length=0 \
+    --max-size=300G --min-age 1m --tpslimit=8 \
+    --drive-pacer-min-sleep=100ms --checkers=8 \
+    --transfers="$vfs_t" --no-traverse \
+    --fast-list --max-transfer "$vfs_mt" --bwlimit="$bwlimit" \
+    --drive-chunk-size="$vfs_dcs" --user-agent="$useragent" \
+    --include="*.srt*" --include="*.idx" --include="*.sub"
+  echo " -- Subs Upload has finished to GDRIVE:--" >>/var/plexguide/logs/pgblitz.log
+  fi
 }
 cloneclean() {
 bash /opt/pgclone/scripts/cloneclean.sh
