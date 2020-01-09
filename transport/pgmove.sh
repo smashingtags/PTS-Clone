0b4175df7b87beebcef48af2ac2a486d194b85f4
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2020-01-09 23:58:51 +0100

    Update pgmove.sh

--- transport/pgmove.sh
+++ transport/pgmove.sh
@@ -13,14 +13,12 @@ echo "---Starting Move: $(date "+%Y-%m-%d %H:%M:%S")---" >>/var/plexguide/logs/p
 
 startscript() {
 while true; do
-
      rsync "$(cat /var/plexguide/server.hd.path)/downloads/" "$(cat /var/plexguide/server.hd.path)/move/" \
       -aq --remove-source-files --link-dest="$(cat /var/plexguide/server.hd.path)/downloads/" \
-      --exclude-from="/opt/pgclone/transport/transport-tdrive.exclude" \
+      --exclude-from="/opt/pgclone/transport/transport-gdrive.exclude" \
       --exclude-from="/opt/pgclone/excluded/excluded.folder"
       if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f | wc -l ) -lt 1 ]]; then rczero; fi
       if [[ $(find "$(cat /var/plexguide/server.hd.path)/move" -type f | wc -l ) -gt 1 ]]; then rcupload; fi
-
  done
  cloneclean
  startscript
@@ -33,14 +31,14 @@ rcupload() {
    let "cyclecount++"
    if [[ $cyclecount -gt 4294967295 ]]; then cyclecount=0; fi
 
-    echo "" >>/var/plexguide/logs/pgblitz.log
-    echo " -- Starting Move Upload cycle $cyclecount - $(date "+%Y-%m-%d %H:%M:%S") --" >>/var/plexguide/logs/pgblitz.log
+    echo "" >>/var/plexguide/logs/pgmove.log
+    echo " -- Starting Move Upload cycle $cyclecount - $(date "+%Y-%m-%d %H:%M:%S") --" >>/var/plexguide/logs/pgmove.log
 
     useragent="$(cat /var/plexguide/uagent)"
     bwlimit="$(cat /var/plexguide/move.bw)"
     vfs_dcs="$(cat /var/plexguide/vfs_dcs)"
 
-    rclone move "$(cat /var/plexguide/server.hd.path)/move/" "gdrive:/" \
+    rclone move "$(cat /var/plexguide/server.hd.path)/move/" "{{type}}:/" \
        --config=/opt/appdata/plexguide/rclone.conf \
        --log-file=/var/plexguide/logs/pgmove.log \
        --log-level=INFO --stats=5s --stats-file-name-length=0 \
@@ -51,7 +49,6 @@ rcupload() {
     echo " -- Completed complete for cycle $cyclecount: $(date "+%Y-%m-%d %H:%M:%S")---" >>/var/plexguide/logs/pgmove.log
     echo "$(tail -n 200 /var/plexguide/logs/pgmove.log)" >/var/plexguide/logs/pgmove.log
 }
-
 cloneclean() {
 bash /opt/pgclone/scripts/cloneclean.sh
 }
