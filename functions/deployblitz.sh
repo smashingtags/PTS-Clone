355595ccfa0629c6764ed1bc1d2d754b2b94d9b5
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2020-01-17 18:19:44 +0100

    [ FIX ] Layout Fix
    [ Fix ] layout
    [ add ] docker uploader when not exist deploy

--- functions/deployblitz.sh
+++ functions/deployblitz.sh
@@ -9,44 +9,32 @@
 # Variable recall comes from /functions/variables.sh
 ################################################################################
 executeblitz() {
-
     # Reset Front Display
     rm -rf plexguide/deployed.version
-
     # Call Variables
     pgclonevars
-
     # flush and clear service logs
     cleanlogs
-    # This must be called before docker apps are stopped!
-    #prunedocker
-
     # to remove all service running prior to ensure a clean launch
     ansible-playbook /opt/pgclone/ymls/remove.yml
-
     cleanmounts
     buildrcloneenv
-
     # gdrive deploys by standard
     echo "tdrive" >/var/plexguide/deploy.version
     echo "bu" >/var/plexguide/deployed.version
     type=gdrive
     encryptbit=""
     ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gdrive"
-
     type=tdrive
     ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=tdrive"
-
     # deploy only if using encryption
     if [[ "$(cat /var/plexguide/pgclone.transport)" == "be"  ]]; then
         ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gcrypt"
-
         echo "be" >/var/plexguide/deployed.version
         type=tcrypt
         encryptbit="C"
         ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=tcrypt"
     fi
-
     # builds the list
     ls -la /opt/appdata/plexguide/.blitzkeys/ | awk '{print $9}' | tail -n +4 | sort | uniq >/var/plexguide/.blitzlist
     rm -rf /var/plexguide/.blitzfinal 1>/dev/null 2>&1
@@ -55,11 +43,12 @@ executeblitz() {
         echo $p >/var/plexguide/.blitztemp
         if [[ "$(grep "GDSA" /var/plexguide/.blitztemp)" != "" ]]; then echo $p >>/var/plexguide/.blitzfinal; fi
     done </var/plexguide/.blitzlist
-
     # deploy union
     ansible-playbook /opt/pgclone/ymls/pgunion.yml -e "transport=$transport type=$type multihds=$multihds encryptbit=$encryptbit"
-    # ansible-playbook /opt/pgclone/ymls/uploader.yml
-
+    upper=$(docker ps --format '{{.Names}}' | grep "uploader")
+    if [[ "$upper" != "uploader" ]]; then 
+       dduploader
+    else ddredeploy; fi
     # check if services are active and running
     failed=false
 
--- functions/deploymove.sh
+++ functions/deploymove.sh
@@ -10,49 +10,39 @@
 ################################################################################
 
 executemove() {
-
     # Reset Front Display
     rm -rf plexguide/deployed.version
-
     # Call Variables
     pgclonevars
-
     # flush and clear service logs
     cleanlogs
-
-    # This must be called before docker apps are stopped!
-    #prunedocker
-
     # to remove all service running prior to ensure a clean launch
     ansible-playbook /opt/pgclone/ymls/remove.yml
-
     cleanmounts
     buildrcloneenv
-    
     # gdrive deploys by standard
     echo "gdrive" >/var/plexguide/deploy.version
     echo "mu" >/var/plexguide/deployed.version
     type=gdrive
     ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gdrive"
-
     # deploy only if pgmove is using encryption
     if [[ "$transport" == "me" ]]; then
         echo "me" >/var/plexguide/deployed.version
         type=gcrypt
         ansible-playbook /opt/pgclone/ymls/drive.yml -e "drive=gcrypt"
     fi
-
     # deploy union
     ansible-playbook /opt/pgclone/ymls/pgunion.yml -e "transport=$transport multihds=$multihds type=$type"
-    # ansible-playbook /opt/pgclone/ymls/uploader.yml
+    upper=$(docker ps --format '{{.Names}}' | grep "uploader")
+    if [[ "$upper" != "uploader" ]]; then 
+       dduploader
+    else ddredeploy; fi
     # output final display
     if [[ "$type" == "gdrive" ]]; then
         finaldeployoutput="Move - Unencrypted"
     else finaldeployoutput="Move - Encrypted"; fi
-
     # check if services are active and running
     failed=false
-
     gdrivecheck=$(systemctl is-active gdrive)
     gcryptcheck=$(systemctl is-active gcrypt)
     pgunioncheck=$(systemctl is-active pgunion)
